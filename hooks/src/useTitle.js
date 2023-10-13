332f20ade9c85b547093a5e7d2fad88791707979
RanungPark <ckehfqkr302@naver.com>
2023-10-13 16:43:48 +0900

    useTitle 훅스 만들기

---
+++ hooks/src/useTitle.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+export const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerHTML = title;
+  };
+
+  useEffect(updateTitle, [title]);
+
+  return setTitle;
+};
