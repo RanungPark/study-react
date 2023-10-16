b1293efddd6d56d2232a69df4064613a2558fa1f
RanungPark <ckehfqkr302@naver.com>
2023-10-16 19:07:57 +0900

    make useNotification Hooks

---
+++ hooks/src/useNotification.js
@@ -0,0 +1,18 @@
+export const useNotification = (title, option) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+  const fireNotif = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title.option);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, option);
+    }
+  };
+};
