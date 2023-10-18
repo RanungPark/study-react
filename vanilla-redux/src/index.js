78a49edb694c5b9f24e708a5dc46ffa6521740fc
RanungPark <ckehfqkr302@naver.com>
2023-10-18 16:38:48 +0900

    바닐라 js 로 투두 작성

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,40 +1,55 @@
 import {  legacy_createStore as createStore } from 'redux';
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-number.innerText = 0;
+// number.innerText = 0;
 
-const ADD = "ADD";
-const MINUS = "MINUS";
+// const ADD = "ADD";
+// const MINUS = "MINUS";
 
-const countModifier = (count = 0, action) => {
-  switch(action.type) {
-    case ADD :
-      return count + 1;
-    case MINUS :
-      return count - 1;
-    default :
-      return count;
-  }
-};
+// const countModifier = (count = 0, action) => {
+//   switch(action.type) {
+//     case ADD :
+//       return count + 1;
+//     case MINUS :
+//       return count - 1;
+//     default :
+//       return count;
+//   }
+// };
 
-const countStore = createStore(countModifier);
+// const countStore = createStore(countModifier);
 
-const onChange = () => {
-  number.innerText = countStore.getState();
+// const onChange = () => {
+//   number.innerText = countStore.getState();
+// }
+
+// countStore.subscribe(onChange);
+
+// const handleAdd = () => {
+//   countStore.dispatch({type: ADD});
+// }
+
+// const handleMinus = () => {
+//   countStore.dispatch({type: MINUS});
+// }
+
+// add.addEventListener('click', handleAdd);
+// minus.addEventListener('click', handleMinus);
+
+const createTodo = toDo => {
+  const li = document.createElement('li');
+  li.innerText = toDo;
+  ul.appendChild(li)
 }
 
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-  countStore.dispatch({type: ADD});
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = '';
+  createTodo(toDo)
 }
 
-const handleMinus = () => {
-  countStore.dispatch({type: MINUS});
-}
-
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+form.addEventListener('submit', onSubmit);
