f6c36c13eec72f08a037c9e4188c1d4728a3fd80
RanungPark <ckehfqkr302@naver.com>
2023-10-20 00:09:33 +0900

    디테일 라우터

--- react-redux/src/routers/Detail.js
+++ react-redux/src/routers/Detail.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-const Detail = () => {
+const Detail = ({toDos}) => {
+  const id = useParams().id
+  const toDo = toDos.find((toDo) => toDo.id === parseInt(id));
   return (
     <div>
-      Detail
+      {toDo?.text}
+      <div>
+      Created at: {toDo?.id}
+      </div>
     </div>
   );
 };
 
-export default Detail;
+const mapStateToProps = (state) => {
+  return {toDos : state}
+}
+
+export default connect(mapStateToProps) (Detail);
