30563627b790c4881603a155b3f4862f5c3dc1f3
RanungPark <ckehfqkr302@naver.com>
2023-10-20 03:03:05 +0900

    루트 리듀서와 데브툴 적용

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { Provider } from 'react-redux';
-import store from './store';
+import {legacy_createStore as createStore } from 'redux';
+import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools()
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
