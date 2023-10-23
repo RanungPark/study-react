ac77627326c2809c907b399c7d208447013c9bc8
RanungPark <ckehfqkr302@naver.com>
2023-10-23 16:14:04 +0900

    attrs로 컴포넌트의 속성 부여

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -4,17 +4,14 @@ const Father = styled.div`
   display: flex;
 `
 
-const Btn = styled.button`
+const Input = styled.input.attrs({required: true, maxLength: 10})`
   background-color: tomato;
-  color: white;
-  border: none;
-  border-radius: 15px;
 `
 export default function App() {
   return (
     <Father>
-      <Btn>Log in</Btn>
-      <Btn as='a' href='/'>Log in</Btn>
+      <Input />
+      <Input />
     </Father>
   );
 }
