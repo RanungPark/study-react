48f49f8f0f066ee64db60670cc37ba3aabecea6a
RanungPark <ckehfqkr302@naver.com>
2023-10-24 08:18:37 +0900

    theme을 이용하여 다크모드 임시 구현

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -6,54 +6,16 @@ const Wrapper = styled.div`
   width: 100vw;
   align-items: center;
   justify-content: center;  
+  background-color: ${props => props.theme.backgroundColor}
 `
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0;
-  }
-  50% {
-    border-radius: 50px;
-  }
-
-  100% {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-`
-
-const Emoji = styled.span`
-  font-size: 2rem;
-`
-
-const Box = styled.div`
-  background-color: tomato;
-  width: 200px;
-  height: 200px;
-  animation:  ${rotationAnimation} 1s linear infinite;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  ${Emoji} {
-    transition: font-size 1s ease-in-out, opacity 1s ease-in-out;
-
-    &:hover {
-      font-size: 5rem;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+const Title = styled.h1`
+  color : ${props => props.theme.textColor};
 `
 
 export default function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😊</Emoji>
-      </Box>
+      <Title> Hello</Title>
     </Wrapper>
   );
 }
