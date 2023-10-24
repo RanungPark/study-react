ad538d6e67a89eea914de342840fe7375c2f6a94
RanungPark <ckehfqkr302@naver.com>
2023-10-24 14:01:09 +0900

    폼이벤트 인터페이스와 제네릭 설정

--- react-typescript-masterclass/src/App.tsx
+++ react-typescript-masterclass/src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import Circle from './Circle';
+import React, { FormEvent, useState } from 'react';
 
 const App = () => {
+  const [value, setValue] = useState("");
+
+  const onChange = (e : FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget : {value}
+    } = e
+    setValue(value);
+  }
+
+  const onSubmit = (e : FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('hello' + value);
+  }
+
   return (
     <div>
-      <Circle bgColor='tomato' borderColor='blue'/>
-      <Circle bgColor='teal' text='im here'/>
+      <form onSubmit={onSubmit}>
+        <input 
+          value={value}
+          onChange={onChange}
+          placeholder='username'
+          type='text'
+        />
+        <button>log in</button>
+      </form>
     </div>
   );
 };
