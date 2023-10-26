20b0bec89b55360c13be4d82d0998376ae5e9d7b
RanungPark <ckehfqkr302@naver.com>
2023-10-27 06:42:24 +0900

    라이브러리추가

--- react-typescript-masterclass/src/index.tsx
+++ react-typescript-masterclass/src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {ThemeProvider} from 'styled-components';
-import { Theme } from './theme';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const root = ReactDOM.createRoot(
@@ -13,9 +11,7 @@ const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={Theme}>
         <App />
-      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
