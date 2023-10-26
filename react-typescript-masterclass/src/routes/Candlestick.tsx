085b8b9301d160fe3b1ecb56cf3d22ad4829da80
RanungPark <ckehfqkr302@naver.com>
2023-10-27 06:42:38 +0900

    Candlestick 컴포넌트

---
+++ react-typescript-masterclass/src/routes/Candlestick.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import ApexChart from 'react-apexcharts';
+import { fetchCoinHistory } from '../api';
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: string;
+}
+
+interface ICandlesticProps {
+  coinId: string;
+}
+
+const Candlestick = ({coinId}:ICandlesticProps) => {
+  const {isLoading, data} = useQuery<IHistorical[]>(['CandlesickOHLCV', coinId], () =>fetchCoinHistory(coinId))
+
+  const mapOHLCData = data?.map((data) => (
+    {
+      x: new Date(parseFloat(data.time_close) *1000).toUTCString(),
+      y: [data.open, data.high, data.low, data.close]
+    })
+    )
+
+  return (
+    <div>
+      {
+        isLoading ? 'Loading...' : (<ApexChart
+          type='candlestick'
+          series={[{data: mapOHLCData}]as unknown as number[]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              width: 500,
+              height: 300,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: {show: false},
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: {show: false},
+              type: "datetime",
+            },
+            yaxis: {show: false},
+          }}
+        />)
+      }
+    </div>
+  );
+};
+
+export default Candlestick;
