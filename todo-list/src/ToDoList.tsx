7f4b4ddf08bc3901196b76ea7d85fc2a8f7d6589
RanungPark <ckehfqkr302@naver.com>
2023-10-28 16:43:52 +0900

    form 에러 메시지 보내기

--- todo-list/src/ToDoList.tsx
+++ todo-list/src/ToDoList.tsx
@@ -1,14 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from "react-hook-form";
 
+interface IForm {
+  toDo : string;
+}
+
 const ToDoList = () => {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit, formState: {errors}} = useForm<IForm>({defaultValues: {toDo :'todo'}});
+  
+  const onValid = (data : any) => {
+    console.log(data);
+  }
   
   return (
     <div>
-      <form>
-        <input {...register("toDo")} placeholder='Write a to do'/>
+      <form onSubmit={handleSubmit(onValid)}>
+        <input {...register("toDo", {required: 'please write todo', minLength: {value: 5, message:'length to short'}})}placeholder='Write a to do'/>
+        <span>
+          {errors?.toDo?.message}
+        </span>
         <button>
           Add
         </button>
