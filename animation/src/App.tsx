83f6843c9ade101ec11eaae6d1d9c42f2f009490
RanungPark <ckehfqkr302@naver.com>
2023-10-31 16:43:34 +0900

    기본 애니메이션

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -14,14 +14,18 @@ const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `
 
 const App = () => {
   return (
     <Wrapper>
-      <Box />
+      <Box 
+        transition={{delay: 0.5, type: 'spring'}}
+        initial={{scale: 0}}
+        animate={{scale: 1, rotateZ: 360}}
+      />
     </Wrapper>
   );
 };
