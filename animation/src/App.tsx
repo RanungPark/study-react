8fae7948c418d3e9495fb819ccfbe7ee10b1b4e7
RanungPark <ckehfqkr302@naver.com>
2023-11-01 14:39:24 +0900

    layoutId을 통하여 컨포넌트 연결하는 애니메이션

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -9,21 +9,18 @@ const Wrapper = styled(motion.div)`
   justify-content: center;
   align-items: center;
   background:linear-gradient(135deg,rgb(116, 185, 255),rgb(9, 132, 227));
+  gap: 100px;
 `
 
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
+  width: 300px;
+  height: 300px;
+  background-color: rgba(255, 255, 255, 0.2);
   border-radius: 40px;
-  position: absolute;
-  top: 100px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   display: flex;
-  justify-content: center;
   align-items: center;
-  font-size: 25px;
-  font-weight: bold;
+  justify-content: center;
 `
 
 const Circle = styled(motion.div)`
@@ -80,35 +77,20 @@ const boxVariants = {
 }
 
 const App = () => {
-  const [index, setIndex] = useState(1);
-  const [isBack, setIsBack] = useState(false);
+  const [click, setClick] = useState(false);
 
-  const onNext = () => {
-    setIsBack(false)
-    setIndex(prev => prev === 10 ? 10 : prev + 1)
-  }
-
-  const onPrev = () => {
-    setIsBack(true)
-    setIndex(prev => prev === 1 ? 1 : prev - 1)
-  }
+  const onClick = () => setClick(prev => !prev);
 
   return (
-    <Wrapper>
-      <AnimatePresence custom={isBack}>
-        <Box
-          custom={isBack}
-          variants={boxVariants}
-          initial='initial'
-          animate='animate'
-          exit='exit'
-          key={index}
-        >
-          {index}
+    <Wrapper  onClick={onClick}>
+      <AnimatePresence>
+        <Box>
+          {click ? <Circle layoutId='circle'/> : null}
+        </Box> 
+        <Box>
+          {click ? null : <Circle layoutId='circle'/> }
         </Box> 
       </AnimatePresence>
-      <button onClick={onPrev}>prev</button>
-      <button onClick={onNext}>next</button>
     </Wrapper>
   );
 };
