bdeca2f178a4298239300d67b4c3904a5824e883
RanungPark <ckehfqkr302@naver.com>
2023-11-02 00:37:23 +0900

    라우터 설정

---
+++ netflix/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
+import Home from './Routers/Home';
+import Tv from './Routers/Tv';
+import Search from './Routers/Search';
+import Header from './Components/Header';
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path='/tv'>
+          <Tv />
+        </Route>
+        <Route path='/search'>
+          <Search />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
