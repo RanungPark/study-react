74c1f402dacedd4de1aec78db565a075dfaa0160
RanungPark <ckehfqkr302@naver.com>
2023-11-04 17:10:33 +0900

    기본과정 종료

--- netflix/src/Routers/Search.tsx
+++ netflix/src/Routers/Search.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import { useLocation } from "react-router";
 
-const Search = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
-
+function Search() {
+  const location = useLocation();
+  const keyword = new URLSearchParams(location.search).get("keyword");
+  console.log(keyword);
+  return null;
+}
 export default Search;
