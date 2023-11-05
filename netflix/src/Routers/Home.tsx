4232331a4fd2d50b9e3ddcabf8df02ac74495c69
RanungPark <ckehfqkr302@naver.com>
2023-11-06 03:43:19 +0900

    subView 컴포넌트 추가

--- netflix/src/Routers/Home.tsx
+++ netflix/src/Routers/Home.tsx
@@ -1,11 +1,9 @@
 import { useQuery } from "react-query";
 import styled from "styled-components";
-import { motion, AnimatePresence,  useScroll } from "framer-motion";
 import { getPopularMovies, getNowPlayingMovies, getTopRatedMovies, getUpcomingMovies, IGetMoviesResult } from "../api";
 import { makeImagePath } from "../utils";
-import { useState } from "react";
-import { useHistory, useRouteMatch } from "react-router-dom";
 import Slider from '../Components/Slider';
+import SubView from '../Components/SubView';
 
 const Wrapper = styled.div`
   background: black;
@@ -47,133 +45,10 @@ const SliderBox = styled.div`
   gap: 250px;
 `;
 
-const Row = styled(motion.div)`
-  display: grid;
-  gap: 5px;
-  grid-template-columns: repeat(6, 1fr);
-  position: absolute;
-  width: 100%;
-`;
-
-const Box = styled(motion.div)<{ bgPhoto: string }>`
-  background-color: white;
-  background-image: url(${(props) => props.bgPhoto});
-  background-size: cover;
-  background-position: center center;
-  height: 200px;
-  font-size: 66px;
-  cursor: pointer;
-  &:first-child {
-    transform-origin: center left;
-  }
-  &:last-child {
-    transform-origin: center right;
-  }
-`;
-
-const Info = styled(motion.div)`
-  padding: 10px;
-  background-color: ${(props) => props.theme.black.lighter};
-  opacity: 0;
-  position: absolute;
-  width: 100%;
-  bottom: 0;
-  h4 {
-    text-align: center;
-    font-size: 18px;
-  }
-`;
-
-const Overlay = styled(motion.div)`
-  position: fixed;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  opacity: 0;
-`;
-
-const BigMovie = styled(motion.div)`
-  position: absolute;
-  width: 40vw;
-  height: 80vh;
-  left: 0;
-  right: 0;
-  margin: 0 auto;
-  border-radius: 15px;
-  overflow: hidden;
-  background-color: ${(props) => props.theme.black.lighter};
-`;
-
-const BigCover = styled.div`
-  width: 100%;
-  background-size: cover;
-  background-position: center center;
-  height: 400px;
-`;
-
-const BigTitle = styled.h3`
-  color: ${(props) => props.theme.white.lighter};
-  padding: 20px;
-  font-size: 46px;
-  position: relative;
-  top: -80px;
-`;
-
-const BigOverview = styled.p`
-  padding: 20px;
-  position: relative;
-  top: -80px;
-  color: ${(props) => props.theme.white.lighter};
-`;
-
-const rowVariants = {
-  hidden: {
-    x: window.outerWidth + 5,
-  },
-  visible: {
-    x: 0,
-  },
-  exit: {
-    x: -window.outerWidth - 5,
-  },
-};
-
-const boxVariants = {
-  normal: {
-    scale: 1,
-  },
-  hover: {
-    scale: 1.3,
-    y: -80,
-    transition: {
-      delay: 0.5,
-      duaration: 0.1,
-      type: "tween",
-    },
-  },
-};
-
-const infoVariants = {
-  hover: {
-    opacity: 1,
-    transition: {
-      delay: 0.5,
-      duaration: 0.1,
-      type: "tween",
-    },
-  },
-};
-
-const offset = 6;
 
 const Home = () => {
-  const history = useHistory();
   const movieTitles = ["now_playing", "popular", "top_rated", "upcoming",]
-  const bigMovieMatch = useRouteMatch<{ movieId: string }>("/movies/:movieId");
   
-  const { scrollY } = useScroll();
-
   const { data: nowPlayingData, isLoading: nowPlayingLoading } = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"],
     getNowPlayingMovies
@@ -193,14 +68,6 @@ const Home = () => {
     ["movies", "upcoming"],
     getUpcomingMovies
   );
-  
-  
-  
-  const onOverlayClick = () => history.push("/");
-
-  const clickedMovie =
-    bigMovieMatch?.params.movieId &&
-    nowPlayingData?.results.find((movie) => movie.id === +bigMovieMatch.params.movieId);
 
   const movieLoading = nowPlayingLoading || popularisLoading || topRatedisLoading || upcomingisLoading
 
@@ -211,46 +78,21 @@ const Home = () => {
       ) : (
         <>
           <Banner
-            bgPhoto={makeImagePath(nowPlayingData?.results[0].backdrop_path || "")}
+            bgPhoto={makeImagePath(popularData?.results[0].backdrop_path || "")}
           >
-            <Title>{nowPlayingData?.results[0].title}</Title>
-            <Overview>{nowPlayingData?.results[0].overview}</Overview>
+            <Title>{popularData?.results[0].title}</Title>
+            <Overview>{popularData?.results[0].overview}</Overview>
           </Banner>
           <SliderBox>
-            <Slider data={nowPlayingData} title={movieTitles[0]}/>
-            <Slider data={popularData} title={movieTitles[1]}/>
-            <Slider data={topRatedData} title={movieTitles[2]}/>
-            <Slider data={upcomingData} title={movieTitles[3]}/>
+            <Slider data={nowPlayingData} title={movieTitles[0]} type='movies'/>
+            <Slider data={popularData} title={movieTitles[1]} type='movies'/>
+            <Slider data={topRatedData} title={movieTitles[2]} type='movies'/>
+            <Slider data={upcomingData} title={movieTitles[3]} type='movies'/>
           </SliderBox>
-          <AnimatePresence>
-            {bigMovieMatch ? (
-              <>
-                <Overlay
-                  onClick={onOverlayClick}
-                  exit={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                />
-                <BigMovie
-                  style={{ top: scrollY.get() + 100 }}
-                  layoutId={bigMovieMatch.params.movieId}
-                >
-                  {clickedMovie && (
-                    <>
-                      <BigCover
-                        style={{
-                          backgroundImage: `linear-gradient(to top, black, transparent), url(${makeImagePath(
-                            clickedMovie.backdrop_path
-                          )})`,
-                        }}
-                      />
-                      <BigTitle>{clickedMovie.title}</BigTitle>
-                      <BigOverview>{clickedMovie.overview}</BigOverview>
-                    </>
-                  )}
-                </BigMovie>
-              </>
-            ) : null}
-          </AnimatePresence>
+          <SubView  data={nowPlayingData} title={movieTitles[0]} type='movies'/>
+          <SubView  data={popularData} title={movieTitles[1]} type='movies'/>
+          <SubView  data={topRatedData} title={movieTitles[2]} type='movies'/>
+          <SubView  data={upcomingData} title={movieTitles[3]} type='movies'/>
         </>
       )}
     </Wrapper>
