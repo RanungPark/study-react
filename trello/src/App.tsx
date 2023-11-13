4e86b17d52e34e1306776d97ed820386ddcdff68
RanungPark <ckehfqkr302@naver.com>
2023-11-13 14:49:42 +0900

    기본틀 잡아두기

--- trello/src/App.tsx
+++ trello/src/App.tsx
@@ -1,5 +1,39 @@
 import React from 'react';
 import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd'
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+`;
+
+const Board = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+const Card = styled.div`
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
+  background-color: ${(props) => props.theme.cardColor};
+`;
+
+const toDos = ["a", "b", "c", "d", "e", "f"];
 
 const App = () => {
   const onDragEnd = () => {
@@ -8,26 +42,30 @@ const App = () => {
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId='one'>
-        {
-          (magic) => (<ul ref={magic.innerRef} {...magic.droppableProps}>
-            <Draggable draggableId='first' index={0}>
-              {
-                (magic) => (
-                  <li ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}><span >🔥</span>one</li>
-                )
-              }
-            </Draggable>
-            <Draggable draggableId='second' index={1}>
-              {
-                (magic) => (
-                  <li ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}><span >🔥</span>two</li>
-                )
-              }
-            </Draggable>
-          </ul>)
-        }
-      </Droppable>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId="one">
+            {(magic) => (
+              <Board ref={magic.innerRef} {...magic.droppableProps}>
+                {toDos.map((toDo, index) => (
+                  <Draggable draggableId={toDo} index={index}>
+                    {(magic) => (
+                      <Card
+                        ref={magic.innerRef}
+                        {...magic.dragHandleProps}
+                        {...magic.draggableProps}
+                      >
+                        {toDo}
+                      </Card>
+                    )}
+                  </Draggable>
+                ))}
+                {magic.placeholder}
+              </Board>
+            )}
+          </Droppable>
+        </Boards>
+      </Wrapper>
     </DragDropContext>
   );
 };
